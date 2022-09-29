72c8d52a267b346f0f03738581250ba4a858a383
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-09-28 22:31:56 -0300

    updates

--- vue-cli/modal-project/src/App.vue
+++ vue-cli/modal-project/src/App.vue
@@ -1,32 +1,49 @@
 <template>
   <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me, MF!</button>
+  <Modal/>
+
+  <p class="d-non">Se eu estiver escondido, algo está errado!</p>
 </template>
 
 <script>
+import Modal from "./components/Modal.vue"
 
 export default {
-  name: 'App',
+  components: { Modal },
   data() {
     return {
       title: "(Not) My first vue App"
+    } 
+  },
+  methods: {
+      handleClick() {
+        this.$refs.name.focus();
+        this.$refs.name.classList.add("active")
+        console.log(this.$refs.name);
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
-h1 {
-  display: inline-block; 
-  padding-bottom: 10px;
-  border-bottom: 2px solid red;
-}
+  h1 {
+    display: inline-block; 
+    padding-bottom: 10px;
+    border-bottom: 2px solid red;
+  }
+
+  input.active {
+    outline: 2px solid rgb(54, 182, 241);
+  }
 </style>
---
+++ vue-cli/modal-project/src/components/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="background" @click="removeModal" ref="backgroundModal">
+    <div class="modal">
+        <h1>Hey! I am a modal!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ullam aut, impedit cupiditate laudantium odio dolore harum in cum cumque expedita voluptates omnis exercitationem ipsum placeat. Minima praesentium quae modi.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        removeModal(e) {
+            this.$refs.backgroundModal.classList.add("d-none");
+        }
+    }
+}
+</script> 
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: white;
+        border-radius: 10px;
+    }
+
+    .background {
+        top: 0;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.499);
+    }
+  
+    .modal p {
+        font-style: normal;
+    }
+
+</style>
