35ac2a5bcf716d1e81bb652427a6538610eb22ae
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-10-15 16:40:59 -0300

    vue with typescript, vue animations

---
+++ vue-animations/src/views/About.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="about">
+    <transition 
+      appear
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+import { gsap } from "gsap";
+
+export default {
+  setup() {
+    const showTitle = ref(true);
+
+    const beforeEnter = (el) => {
+      console.log("Before enter");
+      gsap.from(el, {
+        y: -100,
+        opacity: 0,
+      })
+    } 
+    const enter = (el, done) => {
+      console.log("enter");
+      gsap.to(el, {
+        duration: 2,
+        y: 0,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+      })
+    }
+    const afterEnter = () => {
+      console.log("after enter");
+    }
+
+    return { showTitle, beforeEnter, enter, afterEnter }
+  }
+}
+</script>
+
+<style>
+  .about {
+    max-width: 600px;
+    margin: 20px auto;
+  }
+</style>
---
+++ vue-with-typescript/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <Jobs-list :jobs="jobs"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Job from './types/Job'
+import JobsList from './components/JobsList.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { JobsList }, 
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: 1 },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: 2 },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: 3 },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: 4 },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: 5 },
+      { title: 'Web developer', location: 'Home', salary: 55000, id: 6}
+    ])
+    return { jobs }
+  },
+
+});
+</script>
+
+<style>
+
+</style>
